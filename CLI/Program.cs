5c9dc69d00adf8857bfa8366f6b5e67764628d0c
Jan Doskočil <30779172+Dook97@users.noreply.github.com>
2023-06-30 00:00:00 +0000

    decomposition into separate projets

---
+++ CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UGScraper;
+
+namespace CLI;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var scraper = new Scraper();
+        Console.WriteLine(scraper.TestScrape(args[0]));
+    }
+}
--- UGScraper/Program.cs
+++ UGScraper/Program.cs
@@ -6,19 +6,20 @@ using System.Text.Json;
 using System.Text.Json.Nodes;
 using System.Text.RegularExpressions;
 
-namespace ugfzf;
-class Program
+namespace UGScraper;
+
+public class Scraper
 {
-    static void Main(string[] args)
+    public string TestScrape(string url)
     {
         var web = new HtmlWeb();
-        var doc = web.Load(args[0]);
+        var doc = web.Load(url);
         var item = doc.DocumentNode.SelectNodes("//div[@class='js-store']");
         var raw = HttpUtility.HtmlDecode(item.First().Attributes["data-content"].Value);
         var json = JsonSerializer.Deserialize<JsonNode>(raw.AsSpan());
         var text = json["store"]["page"]["data"]["tab_view"]["wiki_tab"]["content"];
         var rgx = new Regex(@"\[[/]?(ch|tab)\]");
         var clean = rgx.Replace(text.ToString(), "");
-        Console.WriteLine(clean);
+        return clean;
     }
 }
