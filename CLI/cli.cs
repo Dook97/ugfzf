f226d80e6d06250f129f4654bad46885b7c62bf4
Jan Doskočil <30779172+Dook97@users.noreply.github.com>
2023-07-02 00:00:00 +0000

    better exception handling

--- CLI/cli.cs
+++ CLI/cli.cs
@@ -6,7 +6,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        var scraper = new Scraper(args[0]);
-        Console.Out.WriteLine(scraper.GetChords());
+        var scraper = new Scraper();
+        try {
+            scraper.LoadData(args[0]);
+            Console.Out.WriteLine(scraper.GetChords());
+        } catch (ScraperException e) {
+            Console.Error.WriteLine($"An error occured: {e.Message}\nexiting");
+            Environment.Exit(1);
+        }
     }
 }
