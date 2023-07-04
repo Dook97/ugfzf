1385b34dd1be15e9d7e0b1167e3953acb878e462
Jan Doskočil <30779172+Dook97@users.noreply.github.com>
2023-07-04 00:00:00 +0000

    sorta functioning prototype

--- CLI/cli.cs
+++ CLI/cli.cs
@@ -1,38 +1,57 @@
 using System;
 using System.Diagnostics;
+using System.Linq;
 using UGScraper;
 
 namespace CLI;
 class Program
 {
-    static void Main(string[] args)
+    static Process MakeFzfProc()
     {
-        SearchScraper scraper = new();
         Process fzfProc = new();
-        SearchScraperRecord[]? results = null;
-
-        try
-        {
-            scraper.LoadData(args[0]);
-            results = scraper.GetSearchResults();
-        }
-        catch (ScraperException e)
-        {
-            Console.Error.WriteLine($"An error occured: {e.Message}");
-            Environment.Exit(1);
-        }
-
         fzfProc.StartInfo.FileName = "fzf";
         fzfProc.StartInfo.Arguments = "-d \";\" --with-nth=2 --nth=1 --border --border-label \" select a song \"";
         fzfProc.StartInfo.RedirectStandardInput = true;
         fzfProc.StartInfo.RedirectStandardOutput = true;
+        return fzfProc;
+    }
+
+    static SearchScraperRecord[] SearchUG(string query)
+    {
+        SearchScraperRecord[]? results = null;
+        try
+        {
+            SearchScraper searchScraper = new();
+            searchScraper.LoadData(query);
+            results = searchScraper.GetSearchResults();
+        }
+        catch (ScraperException e)
+        {
+            Console.Error.WriteLine($"An error occured: {e.Message}");
+            Console.Error.WriteLine(e.StackTrace);
+            Environment.Exit(1);
+        }
+        return results;
+    }
+
+    static string GetFzfUserInput(SearchScraperRecord[] searchResults)
+    {
+        Process fzfProc = MakeFzfProc();
         fzfProc.Start();
 
-        foreach (SearchScraperRecord result in results)
-            fzfProc.StandardInput.WriteLine($"{result.ScrapeUid};{result.SongName} by {result.ArtistName} ({result.Type})");
-        fzfProc.StandardInput.Close();
-
-        string output = fzfProc.StandardOutput.ReadToEnd();
+        string fzfOut;
+        try
+        {
+            foreach (SearchScraperRecord record in searchResults)
+                fzfProc.StandardInput.WriteLine($"{record.ScrapeUid};{record.SongName} by {record.ArtistName} ({record.Type})");
+            fzfProc.StandardInput.Close();
+            fzfOut = fzfProc.StandardOutput.ReadToEnd();
+        }
+        finally
+        {
+            fzfProc.StandardInput.Close();
+            fzfProc.StandardOutput.Close();
+        }
 
         fzfProc.WaitForExit();
 
@@ -44,7 +63,7 @@ class Program
                 Environment.Exit(0);
                 break;
             case 2:
-                Console.Error.WriteLine("fzf error");
+                Console.Error.WriteLine("fzf error - exiting");
                 Environment.Exit(1);
                 break;
             case 130:
@@ -53,6 +72,20 @@ class Program
                 break;
         }
 
-        Console.Write(output);
+        return fzfOut;
+    }
+
+    static void Main(string[] args)
+    {
+        SearchScraperRecord[] searchResults = SearchUG(args[0]);
+        string fzfOut = GetFzfUserInput(searchResults);
+        var searchLookup = searchResults.ToDictionary(i => i.ScrapeUid, i => i);
+
+        uint uid = uint.Parse(fzfOut.Substring(0, fzfOut.IndexOf(';')));
+        SearchScraperRecord r = searchLookup[uid];
+
+        PageScraper contentScraper = new();
+        contentScraper.LoadData(r.TabUrl);
+        Console.WriteLine(contentScraper.GetChords());
     }
 }
